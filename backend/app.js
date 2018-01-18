d47ad2438a6ed1150ed8c46ae499dffddde234dc
Yan Hrachou <yan.hrachou@duallab.com>
2018-01-18 14:42:59 +0300

    Added backend NodeJS with Express

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var currencyCSV = require('./src/service/CurrencyCSV')
+
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/csv', function (req, res, next) {
+    var currency = req.query.currency;
+    var dateFrom = req.query.dateFrom;
+    var dateTo = req.query.dateTo;
+    if (currency && dateFrom && dateTo) {
+        currencyCSV.ReturnCurrencyCSV(res, currency, dateFrom, dateTo);
+    }
+    else {
+        res.json({'Result': 'Invalid request params'});
+    }
+});
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+
+
+module.exports = app;
